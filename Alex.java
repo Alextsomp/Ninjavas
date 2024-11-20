6cc2bb328489be41b824fb281c49620802913a2b
PetrakisGiannis <t8220123@aueb.gr>
2024-11-20 13:27:24 +0200

    change

--- Alex.java
+++ Alex.java
@@ -1,7 +1,5 @@
 public class Alex {
     public static void main(String[] args) {
-        System.out.println("Petrakis Giannis"); // learning git 
+        System.out.println("Petrakis Giannis"); // learning git
     }
 }
-
-
