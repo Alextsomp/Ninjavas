86973a122d103d233fd9ffff6f8a7ad795bb9e3f
PetrakisGiannis <t8220123@aueb.gr>
2024-11-20 13:35:16 +0200

    Deleted dummy classes

--- Efthymia.java
+++
@@ -1,1 +0,0 @@
-public class Efthymia { public static void main(String[] args) { System.out.println(Hello Git); } }
--- Melina.java
+++
@@ -1,5 +0,0 @@
-public class Melina {
-    public static void main(String[] args) {
-        System.out.println("Helli Ninjavas");
-    }
-}
