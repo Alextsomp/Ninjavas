7da9d3da3e678321e1be8ad0b8a6d26f39a7aa98
PetrakisGiannis <t8220123@aueb.gr>
2024-11-25 14:32:33 +0200

    ready for the presentation

--- routegenie/src/main/java/repository/Main.java
+++ routegenie/src/main/java/repository/Main.java
@@ -23,28 +23,28 @@ public class Main {
                 int firstCityIndex = -1;
                 String[] cityNames = CitiesAndDistances.getCities();
                 
-                System.out.println("Hello there fellow" + ANSI_CYAN + " RoadTripper" + ANSI_RESET + "! Welcome to the" + ANSI_CYAN + " RouteGenie " + ANSI_RESET + "application! Let's begin with your new trip!");
+                System.out.println("\nHello there fellow" + ANSI_CYAN + " RoadTripper" + ANSI_RESET + "! Welcome to the" + ANSI_CYAN + " RouteGenie " + ANSI_RESET + "application! Let's begin with your new trip!\n");
                 System.out.println("Please choose your" + ANSI_RED + " starting " + ANSI_RESET + "and" + ANSI_RED + " finishing " + ANSI_RESET +  "city by inserting the number which the city corresponds to from 0 to 14:");
                 
                 for (int i = 0; i < 15; i++) {
-                    System.out.println(ANSI_YELLOW + i + ": " + ANSI_RESET + cityNames[i]);
+                    System.out.println(ANSI_YELLOW + i + ") " + ANSI_RESET + cityNames[i]);
                 }
                 do {
                     firstCityIndex = sc.nextInt(); 
                     if (CitiesAndDistances.checkExistance(cityNames, firstCityIndex)) {
                         citiesChosen.add(firstCityIndex);
                     } else {
-                        System.out.println("Please enter a number that is between" + ANSI_RED + " 0 " + ANSI_RESET + "and" + ANSI_RED + " 14" + ANSI_RESET + ".");
+                        System.out.println("\nPlease enter a number that is between" + ANSI_RED + " 0 " + ANSI_RESET + "and" + ANSI_RED + " 14" + ANSI_RESET + ".");
                     }
                 } while(firstCityIndex > 14 || firstCityIndex < 0 );
                 
-                System.out.println("Choose the cities you want to visit by inserting the number which the city corresponds to from 0 to 14:");
+                System.out.println("\nChoose the cities you want to visit by inserting the number which the city corresponds to from 0 to 14:");
                 do {
                     System.out.println("Available cities:");
                     // Print available cities that have not been chosen
                  for (int i = 0; i < 15; i++) {
                         if (!citiesChosen.contains(i)) {
-                            System.out.println(ANSI_YELLOW + i + ": " + ANSI_RESET + cityNames[i]);
+                            System.out.println(ANSI_YELLOW + i + ") " + ANSI_RESET + cityNames[i]);
                         }
                     }
                     citiesIndex = sc.nextInt();
@@ -55,14 +55,14 @@ public class Main {
                         if (!citiesChosen.contains(citiesIndex)) {
                             citiesChosen.add(citiesIndex);
                         } else {
-                            System.out.println("This city has already been added. Please select a" + ANSI_RED + " different " + ANSI_RESET + "city that is still available.");
+                            System.out.println("\nThis city has already been added. Please select a" + ANSI_RED + " different " + ANSI_RESET + "city that is still available.\n");
                         }
                     } else {
-                        System.out.println("Please enter a number that is between" + ANSI_RED + " 0 " + ANSI_RESET + "and" + ANSI_RED + " 14" + ANSI_RESET + ".");
+                        System.out.println("\n\nPlease enter a number that is between" + ANSI_RED + " 0 " + ANSI_RESET + "and" + ANSI_RED + " 14" + ANSI_RESET + ".");
                     }
                 
                     // Ask if the user wants to continue or finish
-                    System.out.println("Choose another city from the list or enter" + ANSI_RED + " -1 " + ANSI_RESET + "if you have added all the cities you desire already.");
+                    System.out.println("\nChoose another city from the list or enter" + ANSI_RED + " -1 " + ANSI_RESET + "if you have added all the cities you desire already.");
                     
                     // Check if the user entered -1 to stop the loop
                 } while (citiesIndex != -1);
@@ -81,28 +81,31 @@ public class Main {
                 // }
 
                 if (totalDistanceNN > totalDistanceSolver) {
-                    System.out.println("The best algorithm for your trip is" + ANSI_YELLOW + " Solver." + ANSI_RESET);
-                    System.out.println("According to" + ANSI_YELLOW + " Solver" + ANSI_RESET + ", the best route for your trip is: ");
-                for (int i = 0; i < bestRoutesSolver.size(); i++) {
-                    System.out.printf("%d) %s\n", i+1 , cityNames[bestRoutesSolver.get(i)]);
+                    System.out.println("\nThe best algorithm for your trip is" + ANSI_GREEN + " Solver." + ANSI_RESET);
+                    System.out.println("\nAccording to" + ANSI_GREEN + " Solver" + ANSI_RESET + ", the best route for your trip is: ");
+                for (int i = 0; i < bestRoutesSolver.size()-1; i++) {
+                    System.out.printf(ANSI_CYAN + "%d)" + ANSI_RESET + " %s\n", i+1 , cityNames[bestRoutesSolver.get(i)]);
                 }
-                    System.out.println("And the total distance is goint to be: " + ANSI_GREEN + totalDistanceSolver + ANSI_RESET);
+                    System.out.printf("And then back to %s!\n", cityNames[firstCityIndex]);
+                    System.out.println("\nIn this order, the distance covered is" + ANSI_CYAN + " only " + ANSI_RESET + "going to be " + ANSI_GREEN + totalDistanceSolver + ANSI_RESET + " kilometers!\n");
                 
                 } else if (totalDistanceSolver > totalDistanceNN) {
-                    System.out.println("The best algorithm for this case is" + ANSI_YELLOW + " NN." + ANSI_RESET);
-                    System.out.println("According to" + ANSI_YELLOW + " NN" + ANSI_RESET + ", the best route for your trip is: ");
-                for (int i = 0; i < bestRoutesSolver.size(); i++) {
-                    System.out.printf("%d) %s\n", i+1 , cityNames[bestRoutesNN.get(i)]);
+                    System.out.println("\nThe best algorithm for this case is" + ANSI_GREEN + " NN." + ANSI_RESET);
+                    System.out.println("\nAccording to" + ANSI_GREEN + " NN" + ANSI_RESET + ", the best route for your trip is: ");
+                for (int i = 1; i < bestRoutesNN.size()-1; i++) {
+                    System.out.printf(ANSI_CYAN + "%d)" + ANSI_RESET + " %s\n", i , cityNames[bestRoutesNN.get(i)]);
                 }
-                    System.out.println("The total distance for your trip is goint to be: " + ANSI_GREEN + totalDistanceNN + ANSI_RESET);
+                    System.out.printf("And then back to %s!\n", cityNames[firstCityIndex]);
+                    System.out.println("\nIn this order, the distance covered is" + ANSI_CYAN + " only " + ANSI_RESET + "going to be " + ANSI_GREEN + totalDistanceNN + ANSI_RESET + " kilometers!\n");
                 
                 } else {
-                    System.out.println(ANSI_YELLOW + "Both algorithms agree!" + ANSI_RESET);
-                    System.out.println("The best route for your trip is: ");
-                for (int i = 0; i < bestRoutesNN.size(); i++) {
-                    System.out.printf("%d) %s\n", i+1 , cityNames[bestRoutesNN.get(i)]);
+                    System.out.println(ANSI_GREEN + "\nBoth algorithms agree!" + ANSI_RESET);
+                    System.out.println("\nThe best route for your trip is: ");
+                for (int i = 1; i < bestRoutesNN.size()-1; i++) {
+                    System.out.printf(ANSI_CYAN + "%d)" + ANSI_RESET + " %s\n", i , cityNames[bestRoutesNN.get(i)]);
                 }
-                    System.out.println("The total distance for your trip is goint to be: " + ANSI_GREEN + totalDistanceNN + ANSI_RESET);
+                    System.out.printf("And then back to %s!\n", cityNames[firstCityIndex]);
+                    System.out.println("\nIn this order, the distance covered is" + ANSI_CYAN + " only " + ANSI_RESET + "going to be " + ANSI_GREEN + totalDistanceNN + ANSI_RESET + " kilometers!\n");
                 }
                 
                 sc.close();      
