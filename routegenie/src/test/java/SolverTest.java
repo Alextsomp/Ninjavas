342abbea4148946ce5d43ea757e52f465f07fdca
Makis Bahas <t8230102@aueb.gr>
2024-11-27 00:25:39 +0200

    new tests

--- routegenie/src/test/java/SolverTest.java
+++ routegenie/src/test/java/SolverTest.java
@@ -1,5 +1,5 @@
-import org.junit.jupiter.Test;
-import static org.junit.jupiter.Assert;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 import java.util.List;
 import java.beans.Transient;
