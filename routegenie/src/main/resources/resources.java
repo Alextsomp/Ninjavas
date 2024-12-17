884c45cb1824ba271098873a0a998fd9e353a1d2
Efthymia Iordanidi <t8230042@aueb.gr>
2024-12-17 17:07:18 +0200

    javafx

---
+++ routegenie/src/main/resources/resources.java
@@ -0,0 +1,60 @@
+package application;
+	
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+public class Main extends Application {
+	static VBox root;
+	
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			root = new VBox();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			// Set background color for the root container
+            root.setStyle("-fx-background-color: lightblue;"); // Light blue background color
+
+            // Set up the scene
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Colorful Menu Example");
+            primaryStage.show();
+            
+			load();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+	
