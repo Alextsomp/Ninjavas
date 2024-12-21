e44c287da17491cbefcec53b5d393878178ae084
Melina Fatourou <t8230157@aueb.gr>
2024-12-21 16:12:18 +0200

    Changes to NN

--- routegenie/src/main/java/repository/Solver.java
+++ routegenie/src/main/java/repository/Solver.java
@@ -191,22 +191,22 @@ public class Solver {
         int n = selected.size();
         List<Integer> Best = new ArrayList<>();
         Best.add(start); // The first city will be the starting point
-        int poli = start;
-        int thesiMIN = 0;
+        int StartingPoint = start;
+        int NextCity = 0;
         for (int i = 1; i <= n; i++) {
-            thesiMIN = 0;
+            NextCity = 0;
             double MIN = 10000;
             for (int j = 0; j <= n - 1; j++) {
                 if (selected.get(j) != 0) {
-                    if (dist[poli][selected.get(j)] < MIN) {
-                        MIN = dist[poli][selected.get(j)];
-                        thesiMIN = j;
+                    if (dist[StartingPoint][selected.get(j)] < MIN) {
+                        MIN = dist[StartingPoint][selected.get(j)];
+                        NextCity = j;
                     }
                 }
             }
-            Best.add(selected.get(thesiMIN));
-            poli = selected.get(thesiMIN);
-            selected.set(thesiMIN, 0);
+            Best.add(selected.get(NextCity));
+            StartingPoint = selected.get(NextCity);
+            selected.set(NextCity, 0);
         }
         // Best.add(start); // User will eventually return to the starting point
         return Best;
