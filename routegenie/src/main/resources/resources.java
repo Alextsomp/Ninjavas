18104e74a4dd6dbab164f9390096d493ef0e58bb
Efthymia Iordanidi <t8230042@aueb.gr>
2024-12-25 22:50:25 +0200

    menu with pictures

--- routegenie/src/main/resources/resources.java
+++ routegenie/src/main/resources/resources.java
@@ -83,6 +83,11 @@ public class Main extends Application {
 
 		topContent.getChildren().addAll(title, welcome);
 
+		//ImageView to display the starting city image
+		ImageView cityImageView = new ImageView();
+		cityImageView.setFitWidth(300); // Set width for the image
+	    cityImageView.setPreserveRatio(true); // Maintain the aspect ratio
+		
 		Label info = new Label("Please enter your first and last name);");
 
 		Label firstName = new Label("First Name"); // textfields for the user to fill with their name and submit with a
@@ -200,20 +205,62 @@ public class Main extends Application {
 
                 // Disable the checkbox corresponding to the selected starting city
                 switch (selectedCity) {
-                    case "Athens": c1.setDisable(true); break;
-                    case "Thessaloniki": c2.setDisable(true); break;
-                    case "Patras": c3.setDisable(true); break;
-                    case "Ioannina": c4.setDisable(true); break;
-                    case "Tirana": c5.setDisable(true); break;
-                    case "Skopje": c6.setDisable(true); break;
-                    case "Sofia": c7.setDisable(true); break;
-                    case "Podgorica": c8.setDisable(true); break;
-                    case "Bucharest": c9.setDisable(true); break;
-                    case "Belgrade": c10.setDisable(true); break;
-                    case "Sarajevo": c11.setDisable(true); break;
-                    case "Zagreb": c12.setDisable(true); break;
-                    case "Chisinau": c13.setDisable(true); break;
-                    case "Ljubljana": c14.setDisable(true); break;
+                    case "Athens": 
+                    	c1.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://ipanematravels.com/wp-content/uploads/2023/10/Athens-Greece.jpg"));
+                    	break;
+                    case "Thessaloniki": 
+                    	c2.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://www.letsdrive.gr/sites/default/files/Thessaloniki%20750x500.jpg"));
+                    	break;
+                    case "Patras": 
+                    	c3.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://v9c9u8s9.delivery.rocketcdn.me/wp-content/uploads/2021/09/Upper-town-Patras-.jpg"));
+                    	break;
+                    case "Ioannina": 
+                    	c4.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://www.olympion-rehab.com/cache/files/fb10ef16219a69a5a02cb436efdb5ceda2fd0742.jpg"));
+                    	break;
+                    case "Tirana": 
+                    	c5.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://eia476h758b.exactdn.com/wp-content/uploads/2023/08/Tirana-1.jpg"));
+                    	break;
+                    case "Skopje": 
+                    	c6.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://tripjive.com/wp-content/uploads/2024/07/Is-Skopje-worth-visiting.jpg"));
+                    	break;
+                    case "Sofia": 
+                    	c7.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://cdn.getyourguide.com/img/tour/5c5424b437891.jpeg/146.jpg"));
+                    	break;
+                    case "Podgorica": 
+                    	c8.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://assets.enuygun.com/media/lib/570x400/uploads/image/podgorica-59036.jpeg"));
+                    	break;
+                    case "Bucharest": 
+                    	c9.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://www.wanderlustmagazine.com/wp-content/uploads/2023/11/2-palace-of-parliament-at-sunrise-shutterstock_1788823946-web.jpg"));
+                    	break;
+                    case "Belgrade": 
+                    	c10.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://i0.wp.com/mytravelation.com/wp-content/uploads/2023/12/Belgrade-Serbia.jpg"));
+                    	break;
+                    case "Sarajevo": 
+                    	c11.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://web-assets.transavia.com/78ae936f-d39d-01b0-c3ef-dc738304142f/472cea8d-c842-4343-8c2f-1e5ee7f43acd/Sarajevo.jpg"));
+                    	break;
+                    case "Zagreb": 
+                    	c12.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://www.travel.gr/wp-content/uploads/2021/12/zagreb-kentriki-scaled.jpg"));
+                    	break;
+                    case "Chisinau": 
+                    	c13.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://static.independent.co.uk/2021/09/24/16/iStock-1066138896.jpg"));
+                    	break;
+                    case "Ljubljana": 
+                    	c14.setDisable(true); 
+                    	cityImageView.setImage(loadCityImage("https://cdn.britannica.com/79/124479-050-DD359499/Ljubljana-Slovenia.jpg"));
+                    	break;
                 }
             }
         });
@@ -226,9 +273,18 @@ public class Main extends Application {
 		
 		
 		VBox mainLayout = new VBox(20);
-		mainLayout.getChildren().addAll(topContent, nameFields, originCitySelection, cities, submitSelectedCitiesBtn);
+		mainLayout.getChildren().addAll(topContent, nameFields, originCitySelection, cityImageView, cities, submitSelectedCitiesBtn);
 		root.setCenter(mainLayout);
 
+	}
+
+	public Image loadCityImage(String imageUrl) {
+	    try {
+	        return new Image(imageUrl);
+	    } catch (Exception e) {
+	        System.out.println("Image not found: " + imageUrl);
+	        return null;
+	    }
 	}
 
 }
