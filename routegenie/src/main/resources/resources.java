eead5c171f1c2feb2c94af9028a77daa4ca909c6
Efthymia Iordanidi <t8230042@aueb.gr>
2024-12-25 12:35:39 +0200

    done selected cities

--- routegenie/src/main/resources/resources.java
+++ routegenie/src/main/resources/resources.java
@@ -142,6 +142,13 @@ public class Main extends Application {
 
 		CheckBox c14 = new CheckBox("14. Ljubljana");
 
+		CheckBox[] checkboxes = {c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14};
+
+	        // Initially disable all checkboxes
+	        for (CheckBox checkbox : checkboxes) {
+	            checkbox.setDisable(true);
+	        }
+		
 		VBox cities = new VBox(15);
 
 		cities.getChildren().addAll(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14);
@@ -152,122 +159,74 @@ public class Main extends Application {
 
 		cities.getChildren().add(selectedCitiesText);
 
-		EventHandler<ActionEvent> updateSelectedCities = e -> {
+		// Event handler for updating selected cities
+        EventHandler<ActionEvent> updateSelectedCities = e -> {
+            StringBuilder selectedCities = new StringBuilder("Selected Cities: ");
+            for (CheckBox checkbox : checkboxes) {
+                if (checkbox.isSelected()) {
+                    selectedCities.append(checkbox.getText().substring(3)).append(", ");
+                }
+            }
+            String result = selectedCities.toString().replaceAll(", $", "");
+            if (result.equals("Selected Cities:")) {
+                result = "Selected Cities: None";
+            }
+            selectedCitiesText.setText(result);
+        };
 
-			StringBuilder selectedCities = new StringBuilder("Selected Cities: ");
+        for (CheckBox checkbox : checkboxes) {
+            checkbox.setOnAction(updateSelectedCities);
+        }
 
-			if (c1.isSelected())
-				selectedCities.append("Athens, "); // checks if a city is selected
+        // ComboBox for starting city
+        Label originLabel = new Label("Please select your starting city");
+        ComboBox<String> originComboBox = new ComboBox<>();
+        originComboBox.getItems().addAll("Athens", "Thessaloniki", "Patras", "Ioannina", "Tirana", "Skopje", "Sofia", "Podgorica", "Bucharest", "Belgrade", "Sarajevo", "Zagreb", "Chisinau", "Ljubljana");
+        originComboBox.setPromptText("Choose a city");
+        VBox originCitySelection = new VBox(10);
+        originCitySelection.getChildren().addAll(originLabel, originComboBox);
 
-			if (c2.isSelected())
-				selectedCities.append("Thessaloniki, ");
 
-			if (c3.isSelected())
-				selectedCities.append("Patras, ");
+        originComboBox.setOnAction(e -> {
+        	String selectedCity = originComboBox.getValue();
 
-			if (c4.isSelected())
-				selectedCities.append("Ioannina, ");
+            if (selectedCity != null) {
+                System.out.println("Starting City: " + selectedCity);
 
-			if (c5.isSelected())
-				selectedCities.append("Tirana, ");
+                // Enable all checkboxes except the selected starting city
+                for (CheckBox checkbox : checkboxes) {
+                    checkbox.setDisable(false);
+                }
 
-			if (c6.isSelected())
-				selectedCities.append("Skopje, ");
-
-			if (c7.isSelected())
-				selectedCities.append("Sofia, ");
-
-			if (c8.isSelected())
-				selectedCities.append("Podgorica, ");
-
-			if (c9.isSelected())
-				selectedCities.append("Bucharest, ");
-
-			if (c10.isSelected())
-				selectedCities.append("Belgrade, ");
-
-			if (c11.isSelected())
-				selectedCities.append("Sarajevo, ");
-
-			if (c12.isSelected())
-				selectedCities.append("Zagreb, ");
-
-			if (c13.isSelected())
-				selectedCities.append("Chisinau, ");
-
-			if (c14.isSelected())
-				selectedCities.append("Ljubljana, ");
-
-			String result = selectedCities.toString().replaceAll(", $", "");
-
-			if (result.equals("Selected Cities:")) {
-
-				result = "Selected Cities: None";
-
-			}
-
-			selectedCitiesText.setText(result); // updates the text displaying the selected cities
-
-		};
-
-		c1.setOnAction(updateSelectedCities); // Any possible change will trigger the update logic
-
-		c2.setOnAction(updateSelectedCities);
-
-		c3.setOnAction(updateSelectedCities);
-
-		c4.setOnAction(updateSelectedCities);
-
-		c5.setOnAction(updateSelectedCities);
-
-		c6.setOnAction(updateSelectedCities);
-
-		c7.setOnAction(updateSelectedCities);
-
-		c8.setOnAction(updateSelectedCities);
-
-		c9.setOnAction(updateSelectedCities);
-
-		c10.setOnAction(updateSelectedCities);
-
-		c11.setOnAction(updateSelectedCities);
-
-		c12.setOnAction(updateSelectedCities);
-
-		c13.setOnAction(updateSelectedCities);
-
-		c14.setOnAction(updateSelectedCities);
-
-		//create a combobox where the user selects his starting city
-		Label originLabel = new Label("Please select your starting city");
-		ComboBox<String> originComboBox = new ComboBox<>();
-		originComboBox.getItems().addAll("Athens", "Thessaloniki", "Patras", "Ioannina", "Tirana", "Skopje", "Sofia", "Podgorica", "Bucharest", "Belgrade", "Sarajevo", "Zagreb", "Chisinau", "Ljubljana");
-		originComboBox.setPromptText("Choose a city");
-		VBox originCitySelection = new VBox(10);
-		originCitySelection.getChildren().addAll(originLabel, originComboBox);
-		
-		
-		Button submitCitiesBtn = new Button("Submit Cities");
-		
-		// Initially disable the submit button until a starting city is selected
-		submitCitiesBtn.setDisable(true);
-		
-		originComboBox.setOnAction(e -> {
-			// Enable the submit button when a city is selected from the ComboBox
-			submitCitiesBtn.setDisable(false);
-		});
-		
-        submitCitiesBtn.setOnAction(e -> {
-        	// Print the selected starting city from the ComboBox
-			System.out.println("Starting City:" + originComboBox.getValue());
-        	// Print the selected cities text 
-			System.out.println(selectedCitiesText.getText());
+                // Disable the checkbox corresponding to the selected starting city
+                switch (selectedCity) {
+                    case "Athens": c1.setDisable(true); break;
+                    case "Thessaloniki": c2.setDisable(true); break;
+                    case "Patras": c3.setDisable(true); break;
+                    case "Ioannina": c4.setDisable(true); break;
+                    case "Tirana": c5.setDisable(true); break;
+                    case "Skopje": c6.setDisable(true); break;
+                    case "Sofia": c7.setDisable(true); break;
+                    case "Podgorica": c8.setDisable(true); break;
+                    case "Bucharest": c9.setDisable(true); break;
+                    case "Belgrade": c10.setDisable(true); break;
+                    case "Sarajevo": c11.setDisable(true); break;
+                    case "Zagreb": c12.setDisable(true); break;
+                    case "Chisinau": c13.setDisable(true); break;
+                    case "Ljubljana": c14.setDisable(true); break;
+                }
+            }
         });
-        //Add the submit button to the cities section for layout purposes
-		cities.getChildren().add(submitCitiesBtn);
+       
+        // Button to submit the selected cities
+        Button submitSelectedCitiesBtn = new Button("Submit Selected Cities");
+        submitSelectedCitiesBtn.setOnAction(e -> {
+            System.out.println(selectedCitiesText.getText());
+        });
+		
 		
 		VBox mainLayout = new VBox(20);
-		mainLayout.getChildren().addAll(topContent, nameFields, originCitySelection, cities, submitCitiesBtn);
+		mainLayout.getChildren().addAll(topContent, nameFields, originCitySelection, cities, submitSelectedCitiesBtn);
 		root.setCenter(mainLayout);
 
 	}
