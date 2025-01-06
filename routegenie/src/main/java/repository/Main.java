befe4e3fe44629b026907f03ee0ee49aa93e7591
aggelosnikolaidis <t8220105@aueb.gr>
2025-01-06 18:28:22 +0200

    Allages

--- routegenie/src/main/java/repository/Main.java
+++ routegenie/src/main/java/repository/Main.java
@@ -3,28 +3,34 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import main.java.repository.CityDistanceManager;
+import main.java.repository.Comparison;
+import main.java.repository.Menu;
+
 // import main.java.repository.Comparison;
 // import main.java.repository.Comparsion;
 // import main.java.repository.Menu;
 public class Main {
     public static void main(String[] args) throws SQLException {
        
+        DB dbManager = new DB("ninjavas.db"); 
+        CityDistanceManager cityDistanceManager = new CityDistanceManager(dbManager);
+
         //Initiallizing all the necessery objects from the other classes.
         NearestNeighbour nn = new NearestNeighbour();
         Solver svr = new Solver();
         DynamicProgramming dynamicProg = new DynamicProgramming();
-         //DB dbManager = new DB("ninjavas.db");
-        CitiesAndDistances cad = new CitiesAndDistances();
         Comparison comp = new Comparison();
         Menu mn = new Menu();
-        String[] cityNames = CitiesAndDistances.getCities();
-        double[][] distances = cad.distances;
+
+        String[] cityNames = cityDistanceManager.getAllCities();
+        double[][] distances = cityDistanceManager.getDistance();
         int citiesIndex = 0;
         int firstCityIndex = 0;
 
 
         mn.PrintMenu();
-        ArrayList<Integer> citiesChosen = mn.ChooseCities(firstCityIndex, cityNames, citiesIndex);
+        ArrayList<Integer> citiesChosen = mn.ChooseCities(firstCityIndex, citiesIndex);
          // distances = dynamicProg.fetchDistancesFromDB(dbManager, citiesIndex);
 
          //List<Integer> bestRouteSolver = dynamicProg.dp(dbManager, firstCityIndex, citiesChosen);
