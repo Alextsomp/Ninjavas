972cf6803d0b4bc3299c70954389886cff87a023
aggelosnikolaidis <t8220105@aueb.gr>
2025-01-06 17:48:27 +0200

    New menu

--- routegenie/src/main/java/repository/Menu.java
+++ routegenie/src/main/java/repository/Menu.java
@@ -1,7 +1,10 @@
 package repository;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import repository.DB;
 
 public class Menu {
     Scanner sc = new Scanner(System.in);
@@ -11,6 +14,15 @@ public class Menu {
     String ANSI_GREEN = "\u001B[32m";
     String ANSI_CYAN = "\u001B[36m";
     
+    private DB db;
+    private CityDistanceManager cityDistanceManager;
+    
+    public Menu(String databasePath) throws SQLException {
+        // Δημιουργούμε σύνδεση με τη βάση δεδομένων και το CityDistanceManager
+        db = new DB(databasePath);
+        cityDistanceManager = new CityDistanceManager(db);
+    
+    }
     //The first messages of the application
     public void PrintMenu(){
         System.out.println("\nHello there fellow" + ANSI_CYAN + " RoadTripper" + ANSI_RESET + "! Welcome to the" + ANSI_CYAN + " RouteGenie " + ANSI_RESET + "application! Let's begin with your new trip!\n");
@@ -18,52 +30,55 @@ public class Menu {
     }
     
     //This method is used to add only the available cities you want to visit in a list
-    public ArrayList<Integer> ChooseCities(int firstCityIndex, String[] cityNames, int citiesIndex){
-        CitiesAndDistances cad2 = new CitiesAndDistances();
+    public ArrayList<Integer> ChooseCities(int firstCityIndex, int citiesIndex){
+        
         ArrayList<Integer> citiesChosen = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            System.out.println(ANSI_YELLOW + i + ") " + ANSI_RESET + cityNames[i]);
+        String[] cityList = cityDistanceManager.getAllCities();
+
+        for (int i = 0; i < cityList.length; i++) {
+            System.out.println(ANSI_YELLOW + i + ") " + ANSI_RESET + cityList[i]);
         }
         do {
-            System.out.println("Please choose your" + ANSI_RED + " starting " + ANSI_RESET + "and" + ANSI_RED + " finishing " + ANSI_RESET +  "city by inserting the number which the city corresponds to from 0 to 14:");
-            firstCityIndex = sc.nextInt(); 
-            if (cad2.checkExistance(cityNames, firstCityIndex)) {
+            System.out.println("Please choose your" + ANSI_RED + " starting " + ANSI_RESET + "and" + ANSI_RED + " finishing " + ANSI_RESET +  "city by inserting the number which the city corresponds to:");
+            firstCityIndex = sc.nextInt();
+            if (firstCityIndex >= 0 && firstCityIndex < cityList.length) {
                 citiesChosen.add(firstCityIndex);
             } else {
-                System.out.println("\nPlease enter a number that is between" + ANSI_RED + " 0 " + ANSI_RESET + "and" + ANSI_RED + " 14" + ANSI_RESET + ".");
+                System.out.println("\nPlease enter a valid city index.");
             }
-        } while(firstCityIndex > 14 || firstCityIndex < 0 );
-        System.out.println("\nChoose the cities you want to visit by inserting the number which the city corresponds to from 0 to 14:");
+        } while (firstCityIndex < 0 || firstCityIndex >= cityList.length);
+
+        System.out.println("\nChoose the cities you want to visit by inserting the number which the city corresponds to:");
         do {
             System.out.println("Available cities:");
-            // Print available cities that have not been chosen
-            for (int i = 0; i < 15; i++) {
+            // Εκτυπώνουμε τις πόλεις που δεν έχουν επιλεγεί ήδη
+            for (int i = 0; i < cityList.length; i++) {
                 if (!citiesChosen.contains(i)) {
-                    //Checks if you have already choosen the city you enter
-                    System.out.println(ANSI_YELLOW + i + ") " + ANSI_RESET + cityNames[i]);
+                    System.out.println(ANSI_YELLOW + i + ") " + ANSI_RESET + cityList[i]);
                 }
             }
+
             citiesIndex = sc.nextInt();
             if (citiesIndex == -1) {
-                break; // End input when user enters -1
+                break; // Τερματισμός όταν ο χρήστης εισάγει -1
             }
-            if (cad2.checkExistance(cityNames, citiesIndex)) {
+
+            // Ελέγχουμε αν η πόλη υπάρχει και αν δεν έχει ήδη επιλεγεί
+            if (citiesIndex >= 0 && citiesIndex < cityList.length) {
                 if (!citiesChosen.contains(citiesIndex)) {
                     citiesChosen.add(citiesIndex);
                 } else {
-                    //If you have entered the city prior to now, it throws this message and makes you choose another
                     System.out.println("\nThis city has already been added. Please select a" + ANSI_RED + " different " + ANSI_RESET + "city that is still available.\n");
                 }
             } else {
-                System.out.println("\n\nPlease enter a number that is between" + ANSI_RED + " 0 " + ANSI_RESET + "and" + ANSI_RED + " 14" + ANSI_RESET + ".");
+                System.out.println("\nPlease enter a valid number corresponding to an available city.");
             }
-        
-            // Ask if the user wants to continue or finish
+
+            // Ερώτηση για αν θέλει να συνεχίσει ή να τελειώσει
             System.out.println("\nChoose another city from the list or enter" + ANSI_RED + " -1 " + ANSI_RESET + "if you have added all the cities you desire already.");
-            
-            // Check if the user entered -1 to stop the loop
+
         } while (citiesIndex != -1);
+
         return citiesChosen;
     }
-
 }
